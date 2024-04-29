e0f2bca4b1189b3d0af3a585d4d8e340f65a16a7
Romain <romain.hure@epitech.eu>
2024-04-29 22:38:34 +0200

    feat(src): remove useless file at the root of src.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { PagesModule } from './pages/pages.module';
@@ -21,7 +19,5 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
