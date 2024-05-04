e0e7fae14401952931d31214e25075f415a38b8c
Romain <romain.hure@epitech.eu>
2024-05-04 03:26:18 +0200

    feat(main.ts): add cors, needed to be handle with env var.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-cl
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Licest API')
     .setDescription('The Licest API description')
