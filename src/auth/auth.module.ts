c46e73c8841e03111c0bdcbf8f11dc42ed89937e
Neo-Diamons <romain.hure@epitech.eu>
2024-05-14 17:09:31 +0200

    fix(auth): jwt expire only after a minute.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { JwtModule } from '@nestjs/jwt';
       global: true,
       useFactory: () => ({
         secret: process.env.JWT_SECRET,
-        signOptions: { expiresIn: '60s' },
+        signOptions: { expiresIn: '10d' },
       }),
     }),
   ],
