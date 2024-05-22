33a676de1c1bdd13a04f05d995ce28f3a01e0a6f
Neo-Diamons <romain.hure@epitech.eu>
2024-05-22 11:00:57 +0200

    fix(main): allow cors from all origin.

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-cl
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: `http://localhost:${process.env.FRONTEND_PORT || 5173}`,
-    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  // app.enableCors({
+  //   origin: `http://localhost:${process.env.FRONTEND_PORT || 5173}`,
+  //   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true,
+  // });
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Licest API')
